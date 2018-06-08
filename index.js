720ffa2ae14874302021a43f9fe6cc6647033752
Anubhav Berry <anubhav.berry@gmail.com>
2018-06-08 09:47:10 +0530

    Initial commit as a standalone script with recursive signing and verification capability

--- index.js
+++ index.js
@@ -1,117 +1,76 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-  });
-
-  app.get('/sign', function(req,res){
-
-    var SignedXml = require('xml-crypto').SignedXml	  
-	  , fs = require('fs')
-
-    var xml = "<library>" +
-                "<book id='mybook'>" +
-                  "<name>Harry Potter</name>" +
-                "</book>" +
-              "</library>"
-
-    var sig = new SignedXml()
-    sig.addReference("//*[local-name(.)='book']")   
-    sig.keyInfoProvider = new MyKeyInfo();
-    //sig.signingKey = fs.readFileSync("./certs/test-private/serialnumber-key.pem")
-    sig.signingKey = readPrivateKeyFromProtectedPem('./certs/test-private/serialnumber-key.pem', 'Cl3@n3n3rgy');
-    
-    sig.computeSignature(xml)
-    fs.writeFileSync("output/signed.xml", sig.getSignedXml())
-
-  });
-
-  app.post('/verify', function (req, res) {
-    var select = require('xml-crypto').xpath, 
+var select = require('xml-crypto').xpath, 
         dom = require('xmldom').DOMParser, 
         SignedXml = require('xml-crypto').SignedXml, 
         FileKeyInfo = require('xml-crypto').FileKeyInfo, 
-        fs = require('fs');
-
-        const testFolder = './testfiles/';
-        
-        fs.readdir(testFolder, (err, files) => {
-          files.forEach(file => {
+        fs = require('fs'),
+        forge = require('node-forge')
+        DEBUG = false, FAIL = false, NOFORMATTING = false;
 
 
-            console.log(file);
-            var xml = fs.readFileSync(testFolder + file).toString();
-            var doc = new dom().parseFromString(xml)    
-
-            var signature = select(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0]
-            var certificate = select(doc, "//*[local-name()='X509Certificate']")[0].textContent;
-    
-
-            certificate = "-----BEGIN CERTIFICATE-----\n" + certificate + "\n-----END CERTIFICATE-----";
-            var pemFileName ="certs\\" + file + ".pem"; 
-            fs.writeFile(pemFileName, certificate, function(err) {
-              if(err) {
-                  return console.log(err);
-              }
-          
-              console.log("************" + file );
-
-              var sig = new SignedXml()
-              //sig.keyInfoProvider = new FileKeyInfo("certs\\formbay.public.pem")
-              sig.keyInfoProvider = new FileKeyInfo(pemFileName) ;//"certs\\test.pem")
-              //sig.signingKey= certificate;
-              sig.loadSignature(signature)
-              
-              //console.log(sig);
-              var res = sig.checkSignature(xml)
-              if (!res) {
-                console.log("Failed", sig.validationErrors);
-              } 
-              else {
-                console.log(res + " - Done");
-              };
-            }); 
-              
-
-          });
-        })
-
-
-  var xml = fs.readFileSync("formbayResponse.xml", "utf16le").toString();
- 
-
-
-    res.send('Got a POST request')
-  });
-
-  app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
-  });
-
-  app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user')
-  });
-
-
-  function readPrivateKeyFromProtectedPem(path, passphrase){
-    fs = require('fs');
-    var forge = require('node-forge');
-    var pki = forge.pki;
-
-    var pem = fs.readFileSync(path).toString();
-    var privateKey = pki.decryptRsaPrivateKey(pem, passphrase);
-    return pki.privateKeyToPem(privateKey);
+function sign(dir, file, type) {
+  return new Promise((resolve, reject) => {
+    try {
+      log("Signing [" + dir + "/" + file + "]")
+      var xml = removeFormattingIfRequired(fs.readFileSync(dir + "/" + file, 'utf-8'));
+      var sig = new SignedXml()
+      addReferenceToSignature(sig, type)
+      sig.keyInfoProvider = new MyKeyInfo();
+      sig.canonicalizationAlgorithm = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+      sig.signingKey = readPrivateKeyFromProtectedPem('./certs/test-private/serialnumber-key.pem', 'Cl3@n3n3rgy');
+      sig.computeSignature(xml)
+      fs.writeFileSync(dir.replace('unsigned', 'signed') + "/" + file, sig.getSignedXml())
+      log("Signed [" + dir.replace('unsigned', 'signed') + "/" + file + "]")
+      resolve(dir.replace('unsigned', 'signed') + "/" + file)  
+    } catch(err) {
+      console.error(err)
+      reject(dir.replace('unsigned', 'signed') + "/" + file)
+    }
+  })
 }
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+function verify(file, cert) {
+  try {
+    var xml = failVerificationIfRequired(fs.readFileSync(file, 'utf-8'));
+    var doc = new dom().parseFromString(xml)
+    var signature = select(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0]
+    var sig = new SignedXml();
+    sig.keyInfoProvider = new MyKeyInfo();
+    sig.loadSignature(signature);
+    var res = sig.checkSignature(xml);
+    if (!res) {
+      console.error("ERROR [" + file + "]" + sig.validationErrors);
+    } else {
+      log("SUCCESS " + res);
+    }
+  } catch(err) {
+    console.error("ERROR : " + err)
+  }
+}
 
+
+function addReferenceToSignature(sig, type) {
+  if(type=='product') {
+    sig.addReference("//*[local-name(.)='ProductsVerified']", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"])
+  } else if(type=='installationresponse') {
+    sig.addReference("//*[local-name(.)='InstallationProductVerification']", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"])
+  } else if(type=='recrequest') {
+    sig.addReference("//*[local-name(.)='Registration']", ["http://www.w3.org/TR/2001/REC-xml-c14n-20010315"])
+  } else {
+    sig.addReference("//*[local-name(.)='InstallationProducts']", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"])
+  }
+}
+
+function readPrivateKeyFromProtectedPem(path, passphrase) {
+  var pki = forge.pki;
+  var pem = fs.readFileSync(path).toString();
+  var privateKey = pki.decryptRsaPrivateKey(pem, passphrase);
+  return pki.privateKeyToPem(privateKey);
+}
 
 function MyKeyInfo() {
   this.getKeyInfo = function(key, prefix) {
       prefix = prefix || ''
       prefix = prefix ? prefix + ':' : prefix;
-      console.log("*************** fix this my ");
       var rawX509 = fs.readFileSync("certs/test-private/serialnumber-raw.pem", "utf8");
     return "<" + prefix + "X509Data>"+ rawX509 +"</" + prefix + "X509Data>"
   }
@@ -120,3 +79,53 @@ function MyKeyInfo() {
     return fs.readFileSync("certs/test-private/serialnumber-crt.pem")
   }
 }
+
+
+function signAndVerifyFilesInDirectory(dir) {
+  log("Reading directory [" + dir + "]")
+  createSignedDirectoryIfItDoesNotExist(dir)
+  var files = fs.readdirSync(dir);
+  files.forEach(file => {
+    if (fs.statSync(dir + '/' + file).isDirectory()) {
+      signAndVerifyFilesInDirectory(dir + '/' + file);
+    } else {
+      log("Processing file [" + dir + "/" + file + "]");
+      sign(dir, file, 'product').then((signedFile) => {
+        verify(signedFile)
+      })
+    }
+  });
+};
+
+function createSignedDirectoryIfItDoesNotExist(dir) {
+  var udir = dir.replace('unsigned', 'signed')
+  if(!fs.exists(udir)) {
+    try {
+      fs.mkdirSync(udir);
+    } catch(err) {
+      //console.error(err)
+    }
+  }
+}
+
+function failVerificationIfRequired(xml) {
+  if(FAIL) {
+      xml = xml.replace(/RequestedDateTime/g, "RDT") //uncomment this line to make the verification fail    
+  }
+  return xml;
+}
+
+function removeFormattingIfRequired(xml) {
+  if(NOFORMATTING) {
+    xml = xml.replace(/>\s*</g, '><');
+  }
+  return xml;
+}
+
+function log(msg) {
+  if(DEBUG) {
+    console.log(msg)
+  }
+}
+
+signAndVerifyFilesInDirectory('./unsigned');
